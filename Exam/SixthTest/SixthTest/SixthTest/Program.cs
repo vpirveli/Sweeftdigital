6b325928d4cbea951fc1634870dc5450ef781aa3
Valerian Pirveli <pirveliv@gmail.com>
2021-12-17 01:58:52 +0400

    finished sixth test

---
+++ Exam/SixthTest/SixthTest/SixthTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SixthTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> dictionary = new MyDictionary<string>();
+
+            dictionary.Insert("1");
+            dictionary.Insert("1");
+            dictionary.Insert("2");
+
+            Console.WriteLine(dictionary.Size());
+
+            dictionary.Delete("1");
+            dictionary.Delete("1");
+            dictionary.Delete("2");
+
+            Console.WriteLine(dictionary.Size());
+        }
+    }
+}
